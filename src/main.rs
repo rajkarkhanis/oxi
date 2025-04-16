8b6c8e91ffc6ff6a76ce34e8efd8d1e4e25c9a29
Raj Karkhanis <rkarkhanis00@gmail.com>
2025-04-16 19:15:57 +0530

    basic string-string store

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
-use tokio::net::TcpListener;
-use tokio::io::AsyncWriteExt;
-use tokio::io::AsyncReadExt;
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+
 use tokio::net::TcpStream;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
     println!("Oxi is running on 127.0.0.1:6379");
 
+    let store = Arc::new(Mutex::new(HashMap::<String, String>::new()));
     loop {
-        let (socket, _) = listener.accept().await?;
+        let (mut socket, _) = listener.accept().await?;
         println!("New client connected: {:?}", socket.peer_addr());
 
+        let store = Arc::clone(&store);
+
         tokio::spawn(async move {
-            handle_connection(socket).await;
+            if let Err(e) = socket.write_all("Connected to Oxi!\r\n".as_bytes()).await {
+                eprintln!("Failed to write to socket: {}", e);
+            }
+            handle_connection(socket, store).await;
         });
     }
 }
 
-async fn handle_connection(mut socket: TcpStream)  {
+async fn handle_connection(mut socket: TcpStream, store: Arc<Mutex<HashMap<String, String>>>)  {
     let mut buffer = [0; 1024];
 
     loop {
@@ -29,10 +37,12 @@ async fn handle_connection(mut socket: TcpStream)  {
             }
 
             Ok(n) => {
-                let msg = String::from_utf8_lossy(&buffer[..n]);
-                println!("Received: {}", msg);
+                let input = String::from_utf8_lossy(&buffer[..n]).trim().to_string();
+                println!("Received: {}", input);
 
-                if let Err(e) = socket.write_all(&buffer[..n]).await {
+                let response = process_command(input, &store);
+
+                if let Err(e) = socket.write_all(response.as_bytes()).await {
                     eprintln!("Failed to write to socket: {}", e);
                     break;
                 }
@@ -43,5 +53,39 @@ async fn handle_connection(mut socket: TcpStream)  {
                 break;
             }
         }
+    }
+}
+
+fn process_command(input: String, store: &Arc<Mutex<HashMap<String, String>>>) -> String {
+    let parts: Vec<&str> = input.split_whitespace().collect();
+
+    if parts.is_empty() {
+        return "ERROR: No command entered. Try 'SET key value' or 'GET key'\r\n".to_string();
+    }
+
+    match parts[0].to_uppercase().as_str() {
+        "SET" => {
+            if parts.len() != 3 {
+                return "ERROR: Invalid SET command. Use 'SET key value'\r\n".to_string();
+            }
+
+            let mut db = store.lock().unwrap();
+            db.insert(parts[1].to_string(), parts[2].to_string());
+            "OK\r\n".to_string()
+        }
+
+        "GET" => {
+            if parts.len() != 2 {
+                return "ERROR: Invalid GET command. Use 'GET key'\r\n".to_string();
+            }
+
+            let db = store.lock().unwrap();
+            match db.get(parts[1]) {
+                Some(val) => format!("{}: {}\r\n", parts[1], val),
+                None => "ERROR: Key not found\r\n".to_string(),
+            }
+        }
+
+        _ => "ERROR: Invaid command. Try 'GET key' or 'SET key value'\r\n".to_string()
     }
 }
