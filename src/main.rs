92cb14471083864e8daa3714db2e702f03171307
Raj Karkhanis <rkarkhanis00@gmail.com>
2025-04-16 18:27:43 +0530

    basic echo tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use tokio::net::TcpListener;
+use tokio::io::AsyncWriteExt;
+use tokio::io::AsyncReadExt;
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -10,7 +13,35 @@ async fn main() -> anyhow::Result<()> {
         println!("New client connected: {:?}", socket.peer_addr());
 
         tokio::spawn(async move {
-            println!("hi bro")
+            handle_connection(socket).await;
         });
+    }
+}
+
+async fn handle_connection(mut socket: TcpStream)  {
+    let mut buffer = [0; 1024];
+
+    loop {
+        match socket.read(&mut buffer).await {
+            Ok(0) => {
+                println!("Client disconnected");
+                break;
+            }
+
+            Ok(n) => {
+                let msg = String::from_utf8_lossy(&buffer[..n]);
+                println!("Received: {}", msg);
+
+                if let Err(e) = socket.write_all(&buffer[..n]).await {
+                    eprintln!("Failed to write to socket: {}", e);
+                    break;
+                }
+            }
+
+            Err(e) => {
+                eprintln!("Failed to read from socket: {}", e);
+                break;
+            }
+        }
     }
 }
